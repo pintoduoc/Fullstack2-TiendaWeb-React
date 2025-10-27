cf22c736ed21b9172ae1eedc880572db50b15c1f
doscares <osca.sepulveda@duocuc.cl>
2025-10-27 15:59:58 -0300

    agregue lo del axios y el localhost4000

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,15 @@
-import { products } from "../data/products";
 import ProductCard from "../components/ProductCard";
+import { useEffect, useState } from "react";
 
 export default function Products({ addToCart }) {
+ const [products, setProducts] = useState([])
+ useEffect(()=>{
+  fetch("http://localhost:4000/api/productos")
+  .then((response)=>response.json())
+  .then((data)=> setProducts (data))
+ },
+[])
+
   return (
     <div className="products">
       <h1>Planes de Suscripción</h1>
